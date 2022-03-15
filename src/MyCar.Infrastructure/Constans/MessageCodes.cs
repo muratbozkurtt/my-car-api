7e6aab60fe737668d74de4b578301ec540590ba2
Murat Bozkurt <murat.bozkurt@neyasis.com>
2022-03-15 17:17:34 +0300

    continue

--- src/MyCar.Infrastructure/Constans/MessageCodes.cs
+++ src/MyCar.Infrastructure/Constans/MessageCodes.cs
@@ -2,12 +2,11 @@
 {
     public static class MessageCodes
     {
-        private static string GeneralCodePrefix = "GC-0-";
-        private static string SuccessCodePrefix = "SC-1-";
-        private static string ErrorCodePrefix = "EC-2-";
-        private static string WarningCodePrefix = "WC-3-";
-        private static string InformationCodePrefix = "IC-4-";
-        private static string CandidateCodePrefix = "CC-5-";
+        private static string GeneralCodePrefix = "";
+        private static string SuccessCodePrefix = "";
+        private static string ErrorCodePrefix = "";
+        private static string WarningCodePrefix = "";
+        private static string InformationCodePrefix = "";
 
         public static class General
         {
@@ -15,20 +14,6 @@
             public static string NotFound = GeneralCodePrefix + "1";
             public static string Exist = GeneralCodePrefix + "2";
             public static string NotExist = GeneralCodePrefix + "3";
-            public static string EnteredInfoFalse = GeneralCodePrefix + "4";
-            public static string NotActive = GeneralCodePrefix + "5";
-            public static string ApplicationException = GeneralCodePrefix + "6";
-            public static string EmailNotConfirm = GeneralCodePrefix + "7";
-            public static string EmailNotConfirmed = GeneralCodePrefix + "8";
-            public static string EmailIsAlreadyConfirmed = GeneralCodePrefix + "9";
-            public static string EmailAndUserTypeAlreadyExist = GeneralCodePrefix + "10";
-            public static string ImageTypeNotValid = GeneralCodePrefix + "11";
-            public static string FileTypeNotValid = GeneralCodePrefix + "12";
-            public static string FileSizeNotValid = GeneralCodePrefix + "13";
-            public static string FileNotUploaded = GeneralCodePrefix + "14";
-            public static string UnAuthorized = GeneralCodePrefix + "15";
-            public static string Forbidden = GeneralCodePrefix + "16";
-            public static string NotEmployerActive = GeneralCodePrefix + "17";
         }
 
         public static class Success
@@ -67,13 +52,5 @@
             public static string Deleted = InformationCodePrefix + "4";
         }
 
-        public static class Candidate
-        {
-            public static string UserIdNotMatched = CandidateCodePrefix + "0";
-            public static string UserTypeNotMatched = CandidateCodePrefix + "1";
-            public static string UserNameNotFound = CandidateCodePrefix + "2";
-            public static string EmailUpdated = CandidateCodePrefix + "3";
-            public static string ContactInformationNotFound = CandidateCodePrefix + "4";
-        }
     }
 }
